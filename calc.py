46f51d2bc2f47ba962cd2cc4fb81dcbd69ca09b9
Benjamin S <benjaminsiverly@gmail.com>
2017-02-15 15:57:10 -0700

    Update calc.py
    Removed unused import.
    Corrected ability to find operator and nums.
    Edited add, subtract, multiply and divide to take more than one value - operating from smallest index (first entered) to largest.
    Still not able to combine multiple operations (eg - '1 2 + 3 *')

--- calc.py
+++ calc.py
@@ -1,46 +1,67 @@
-from __future__ import division
-
 class PolishCalc():
     def __init__(self):
-        self.orig_problem = raw_input("numbers in front - spaces between - operator at end.> ").strip().split()
-        self.operator = self.orig_problem.pop(-1)
-        self.nums = self.orig_problem
+        self.nums = []
+        self.answer = 0
+        self.operators = {}
+        self.original_input = []
+        self.input = raw_input("numbers in front - spaces between - operator at end.> ").strip().split()
+        for items in self.input:
+            if items in '+-*/':
+                self.original_input.append(items)
+            elif items in map(str, list(range(1,500000))):
+                self.original_input.append(items)
+        for idx, itm in enumerate(self.original_input):
+            try:
+                self.nums.append(int(itm))
+            except ValueError:
+                pass
+            if itm in '+-/*':
+                self.operators[itm] = idx
 
     def do_maths(self):
-        if self.operator in '+-/*':
-            if self.operator == '+':
-                print(self.addi())
-            elif self.operator == '-':
-                print(self.subt())
-            elif self.operator == '/':
-                print(self.divi())
-            elif self.operator == '*':
-                print(self.mult())
+        for key, value in self.operators.items():
+            if key == '+':
+                (self.addi(value))
+            elif key == '-':
+                (self.subt(value))
+            elif key == '/':
+                (self.divi(value))
+            elif key == '*':
+                (self.mult(value))
+            print(self.answer)
 
-    def addi(self):
-        answer = 0
-        for num in self.nums:
-            answer += int(num)
-        return answer
-
-
-    def subt(self):
-        answer = 0
-        for num in self.nums:
-            answer -= int(num)
-        return answer
+    def addi(self, value):
+        idx_to_add = list(range(0,value))
+        for idxs in idx_to_add:
+            self.answer += (int(self.original_input[idxs]))
+        return self.answer
 
 
-    def mult(self):
-        answer = 1
-        for num in self.nums:
-            answer *= int(num)
-        return answer
+    def subt(self, value):
+        idx_to_add = sorted(list(range(0,value)))
+        for idxs in idx_to_add:
+            if idxs == 0:
+                self.answer = (int(self.original_input[idxs]))
+            else:
+                self.answer -= (int(self.original_input[idxs]))
+        return self.answer
 
-    def divi(self):
-        answer = int(self.nums[0])/int(self.nums[1])
-        return int(answer)
+
+    def mult(self, value):
+        idx_to_add = sorted(list(range(0,value)))
+        for idxs in idx_to_add:
+            if idxs == 0:
+                self.answer = (int(self.original_input[idxs]))
+            else:
+                self.answer *= (int(self.original_input[idxs]))
+        return self.answer
 
 
-problem = PolishCalc()
-problem.do_maths()
+    def divi(self, value):
+        idx_to_add = sorted(list(range(0,value)))
+        for idxs in idx_to_add:
+            if idxs == 0:
+                self.answer = (int(self.original_input[idxs]))
+            else:
+                self.answer = self.answer / (int(self.original_input[idxs]))
+        return self.answer
