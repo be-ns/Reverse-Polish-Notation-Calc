d0516d5abbfd683a67e5e6d730a65bec28f7f3cb
Benjamin S <benjaminsiverly@gmail.com>
2017-03-30 10:39:28 -0600

    Used operator library instead of manual math
    from operator import add, mul, sub, truediv
    shortened script
    improved efficiency

--- calc.py
+++ calc.py
@@ -1,6 +1,4 @@
-from collections import OrderedDict
-import os
-import sys
+from operator import add, mul, sub, truediv
 
 
 class PolishCalc(object):
@@ -8,15 +6,12 @@ class PolishCalc(object):
         '''
         initializes the stack
         '''
-        self.answer = 0
         self.stack = []
         self.input = input('Numbers at the beginning > operator at the end> ').strip()
-        self.maths = ['+','-','/','*']
+        self.maths = ['+', '-', '/', '*']
         self.do_maths()
 
-
     def do_maths(self):
-        # (self.stack).remove(0)
         self.input = [item for item in self.input.split(' ')]
         for item in self.input:
             if item not in self.maths:
@@ -30,58 +25,26 @@ class PolishCalc(object):
                     self.answer = self.divide()
                 elif item == '*':
                     self.answer = self.multiply()
-            # print(self.answer)
         print(self.answer)
-        # ("The solution to your input is {0}.".format(self.answer))
 
     def add(self):
-        self.answer = (sum(self.stack))
-        self.stack = [self.answer]
-        return(self.answer)
+        self.stack.append(add(self.stack.pop(), self.stack.pop()))
+        return(self.stack)
 
     def subtract(self):
-        if self.answer == 0:
-            self.stack.pop(0)
-            for itm in self.stack:
-                self.answer -= itm
-            self.stack = [self.answer]
-        else:
-            self.stack.pop(0)
-            self.answer -= (sum(self.stack))
-            self.stack = [self.answer]
-        return(self.answer)
+        self.stack.append(sub(self.stack.pop(-2), self.stack.pop()))
+        return(self.stack)
 
     def divide(self):
-        if self.answer == 0:
-            self.answer = self.stack.pop(0)
-            for itm in self.stack:
-                self.answer = (self.answer / itm)
-            self.stack = [self.answer]
-            return(self.answer)
-        else:
-            self.answer = self.stack.pop(0)
-            for itm in self.stack:
-                self.answer = (self.answer / float(itm))
-            self.stack = [self.answer]
-        return(self.answer)
+        self.stack.append(truediv(self.stack.pop(-2), float(self.stack.pop())))
+        return(self.stack)
 
     def multiply(self):
-        if self.answer == 0:
-            self.answer = self.stack.pop(0)
-            for itm in self.stack:
-                self.answer = (self.answer * itm)
-            self.stack = [self.answer]
-            return (self.answer)
-        else:
-            self.answer = self.stack.pop(0)
-            for itm in self.stack:
-                self.answer *= itm
-            self.stack = [self.answer]
-        return(self.answer)
+        self.stack.append(mul(self.stack.pop(), self.stack.pop()))
+        return(self.stack)
 
     def __repr__(self):
-        return ("The solution to your input is {0}.").format(self.answer)
+        return "The solution to your input is {0}.".format(self.answer)
 
 if __name__ == '__main__':
     calc = PolishCalc()
- 
