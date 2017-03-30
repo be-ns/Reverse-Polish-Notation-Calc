4a3c487c862abdfaa34f3e31607fb8fc0a4fa5fc
Benjamin S <benjaminsiverly@gmail.com>
2017-03-30 10:05:15 -0600

    Updated
    Corrected functionality

--- calc.py
+++ calc.py
@@ -1,75 +1,86 @@
-class PolishCalc():
+from collections import OrderedDict
+import os
+import sys
+
+
+class PolishCalc(object):
     def __init__(self):
+        '''
+        initializes the stack
+        '''
         self.answer = 0
         self.stack = []
-        self.input = list(raw_input("numbers in front - spaces between - operator at end.> ").strip())
+        self.input = input('Numbers at the beginning > operator at the end> ').strip()
+        self.maths = ['+','-','/','*']
+        self.do_maths()
+
 
     def do_maths(self):
+        # (self.stack).remove(0)
+        self.input = [item for item in self.input.split(' ')]
         for item in self.input:
-            if item in map(str, list(range(1,500000))):
+            if item not in self.maths:
                 self.stack.append(int(item))
-            elif item in '+-*/':
-                if item == '+':
-                    self.addi()
-                elif item == '-':
-                    self.subt()
-                elif item == '/':
-                    self.divi()
-                elif item == '*':
-                    self.multi()
             else:
-                pass
+                if item == '+':
+                    self.answer = self.add()
+                elif item == '-':
+                    self.answer = self.subtract()
+                elif item == '/':
+                    self.answer = self.divide()
+                elif item == '*':
+                    self.answer = self.multiply()
+            # print(self.answer)
+        print(self.answer)
+        # ("The solution to your input is {0}.".format(self.answer))
+
+    def add(self):
+        self.answer = (sum(self.stack))
+        self.stack = [self.answer]
         return(self.answer)
 
-    def addi(self):
-        for itm in self.stack:
-            self.answer  += itm
-        self.stack = [self.answer]
-        return self.stack
-
-    def subt(self):
+    def subtract(self):
         if self.answer == 0:
-            self.answer = self.stack[0]
             self.stack.pop(0)
             for itm in self.stack:
                 self.answer -= itm
             self.stack = [self.answer]
         else:
             self.stack.pop(0)
-            for itm in self.stack:
-                self.answer -= itm
+            self.answer -= (sum(self.stack))
             self.stack = [self.answer]
-        return self.stack
+        return(self.answer)
 
-    def divi(self):
+    def divide(self):
         if self.answer == 0:
-            self.answer = self.stack[0]
-            self.stack.pop(0)
+            self.answer = self.stack.pop(0)
             for itm in self.stack:
-                self.answer = self.answer / itm
+                self.answer = (self.answer / itm)
             self.stack = [self.answer]
+            return(self.answer)
         else:
-            self.stack.pop(0)
+            self.answer = self.stack.pop(0)
             for itm in self.stack:
-                self.answer = self.answer / itm
+                self.answer = (self.answer / float(itm))
             self.stack = [self.answer]
-        return self.stack
+        return(self.answer)
 
-    def multi(self):
+    def multiply(self):
         if self.answer == 0:
-            self.answer = self.stack[0]
-            self.stack.pop(0)
+            self.answer = self.stack.pop(0)
             for itm in self.stack:
-                self.answer = self.answer * itm
+                self.answer = (self.answer * itm)
             self.stack = [self.answer]
+            return (self.answer)
         else:
-            self.stack.pop(0)
+            self.answer = self.stack.pop(0)
             for itm in self.stack:
-                self.answer = self.answer * itm
+                self.answer *= itm
             self.stack = [self.answer]
-        return self.stack
+        return(self.answer)
 
+    def __repr__(self):
+        return ("The solution to your input is {0}.").format(self.answer)
 
-
-problem = PolishCalc()
-print(problem.do_maths())
+if __name__ == '__main__':
+    calc = PolishCalc()
